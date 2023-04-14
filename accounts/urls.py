e19d43c0c0d9a7df5fc17ab99bb555643a7fa76b
aliteymouri <aliteymr@gmail.com>
2023-04-14 16:17:59 +0330

    set base of edit-personal-info

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('sign-in', SignInView.as_view(), name='sign-in'),
     path('sign-up', SignUpView.as_view(), name='sign-up'),
-    path('log-out', LogoutView.as_view(next_page='home:home'), name='log-out'),
+    path('log-out/', LogoutView.as_view(next_page='home:home'), name='log-out'),
 
     path('check-otp', CheckOtpView.as_view(), name='check-otp'),
 
